2aab87f4180e7f2d140c05e8180e780b386eadce
vmatveev <vmatveevspam@gmail.com>
2021-06-25 21:41:02 -0400

    countries step3 done

--- part2/countries/src/components/Country/Country.js
+++ part2/countries/src/components/Country/Country.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API;
 
 const Country = ({data}) => {
-  console.log(data);
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${WEATHER_API_KEY}&query=${data.capital}`)
+      .then(response => {
+        setWeather(response.data.current)
+      })
+  },[data.capital])
+
   let dataToRender;
   if (data) {
     dataToRender = data.languages.map((lang, i) => {
@@ -30,7 +42,18 @@ const Country = ({data}) => {
         style={{
           height:"100px",
           width: "100px"
-        }}/>
+        }}
+      />
+
+      <div>
+        <h3>Weather in {data.capital}</h3>
+        <strong>temperature:</strong> {weather.temperature} Celcius <br />
+        <img 
+          src={weather.weather_icons}
+          alt={'icon'}
+        /> <br />
+        <strong>wind:</strong> {weather.wind_speed} MPH direction {weather.wind_dir}
+      </div>
     </div>
   )
 }
