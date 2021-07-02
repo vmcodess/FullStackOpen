8301c6ff39337cfff972bf8dbffe41638923d5a3
vmatveev <vmatveevspam@gmail.com>
2021-07-02 18:30:35 -0400

    3.9 changed URL to backend (part3)

--- part2/phonebook/src/components/Services/PersonService.js
+++ part2/phonebook/src/components/Services/PersonService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = 'http://localhost:3001/persons'
+const baseURL = 'https://lit-retreat-17374.herokuapp.com/persons'
 
 const getAll = () => {
     const request = axios.get(baseURL)
