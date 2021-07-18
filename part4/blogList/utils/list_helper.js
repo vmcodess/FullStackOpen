7c2e56ba8c9a8be40a0d932870b03d042b066b34
vmatveev <vmatveevspam@gmail.com>
2021-07-18 00:51:35 -0400

    4.5

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -6,7 +6,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accumulator, current) => accumulator + current.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  let mostLikedBlog = blogs.reduce((prevBlog, currentBlog) => prevBlog.likes > currentBlog.likes ? prevBlog : currentBlog);
+  mostLikedBlog = (({ title, author, likes }) => ({ title, author, likes }))(mostLikedBlog);
+  console.log(mostLikedBlog);
+  return mostLikedBlog.likes;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
