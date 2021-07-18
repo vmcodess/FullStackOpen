062ce90037f91c2bfda013cbadcb460cad97d885
vmatveev <vmatveevspam@gmail.com>
2021-07-17 22:30:37 -0400

    4.2

---
+++ part4/blogList/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('Connected to DB!');
+  })
+  .catch((err) => {
+    console.log(`error connecting to db: ${err}`);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
