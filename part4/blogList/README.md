d881924353e7c16252887840d7901b00a45dbdb4
vmatveev <vmatveevspam@gmail.com>
2021-07-23 14:19:33 -0400

    4.9

--- part4/blogList/README.md
+++ part4/blogList/README.md
@@ -1,1 +1,2 @@
-Note: skipping exercise 4.6 & 4.7. Will return to them at the end of part 4.
+Note: 
+- skipping exercise 4.6 & 4.7. Will return to them at the end of part 4.
--- part4/blogList/tests/blog_list.test.js
+++ part4/blogList/tests/blog_list.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest');
 const app = require('../app');
 const api = supertest(app);
 const Blogs = require('../models/blog');
+const helper = require('./test_helper');
 
 const initialBlogs = [
   {
@@ -71,6 +72,13 @@ test('all blogs returned as json', async () => {
     .expect('Content-Type', /application\/json/);
 
   expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+test('query if a blog has id or _id', async () => {
+  const singleBlog = await helper.blogsInDb();
+
+  expect(singleBlog[0].id).toBeDefined();
+  expect(singleBlog[0]._id).toBe(undefined);
 });
 
 afterAll(() => {
